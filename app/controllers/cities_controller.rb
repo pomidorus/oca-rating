0a49a068091b0cdec39203768d6d2d59e1f960db
Andrey Seleznov <aseleznev@sphereconsultinginc.com>
2014-10-10 18:59:04 +0300

    добавление нового города

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,4 +1,19 @@
 class CitiesController < ApplicationController
   def new
+    @city = City.new
   end
+
+  def create
+    @city = City.create(city_params)
+  end
+
+  def show
+  end
+
+  private
+
+  def city_params
+    params.require(:city).permit(:uk_title)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'cities/new'
-
   resources :cities do
     resource :region, only: [:show]
   end
