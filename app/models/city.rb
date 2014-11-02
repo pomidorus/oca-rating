85234a598ff322bd309fae357ed1cfeca3621e7f
Andrey Seleznov <andrewaka39@gmail.com>
2014-11-02 20:28:20 +0200

    фикс. пустая ссылка на веб ресурс в городе

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,12 @@
 class City < ActiveRecord::Base
   belongs_to :region
+
+  def link_decorator
+    if link
+      link.gsub('http://www.', '').chomp('/')
+    else
+      "#{uk_title} has no web link"
+    end
+  end
 end
+
