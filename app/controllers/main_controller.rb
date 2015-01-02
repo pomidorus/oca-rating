e2e728ff3b7e9f5404771ddcf7f6baae92bb1e54
Andrey Seleznov <andreii.seleznov@gmail.com>
2015-01-02 16:32:06 +0200

    номер города в рейтинге

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,15 +1,16 @@
 class MainController < ApplicationController
   def index
     @regions ||= Region.all
-
     @cities ||= City.all
+
     @cities = City.joins(:region).where('regions.uk_name = ?', params[:region]) if params[:region].present?
 
-    @rating = 0
-    @cities.each do |city|
-      @rating += CityRating.calculate city
-    end
-
-    @rating_all = CityRating.total_calculate @cities
+    @city_rating = CityRating.new @cities
+    # @rating = 0
+    # @cities.each do |city|
+    #   @rating += CityRating.calculate city
+    # end
+    #
+    # @rating_all = CityRating.total_calculate @cities
   end
 end
--- app/models/city_rating.rb
+++ app/models/city_rating.rb
@@ -1,7 +1,26 @@
 class CityRating
   RATING_ATTRIBUTES = %w{link?}
 
-  def self.calculate city
+  def initialize cities
+    @cities = cities
+    @rating = {}
+    calculate_rating
+  end
+
+  def position_of city
+    @rating[city.id]
+  end
+
+  private
+
+  def calculate_rating
+    count = []
+    @cities.each { |city| count << {city.id => calculate(city)} }
+    count.sort! {|x,y| y.values[0] <=> x.values[0]}
+    count.each_with_index { |c, index| @rating[c.keys[0]] = index + 1 }
+  end
+
+  def calculate city
     count = 0
     RATING_ATTRIBUTES.each do |attr|
       present = city.send("#{attr}")
@@ -10,14 +29,14 @@ class CityRating
     count
   end
 
-  def self.total_calculate cities
-    count = 0
-    cities.each do |city|
-      RATING_ATTRIBUTES.each do |attr|
-        count += 10
-      end
-    end
-    count
-  end
+  # def self.total_calculate cities
+  #   count = 0
+  #   cities.each do |city|
+  #     RATING_ATTRIBUTES.each do |attr|
+  #       count += 10
+  #     end
+  #   end
+  #   count
+  # end
 
 end
