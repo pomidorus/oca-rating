db996d7d8be98da09dd6911c9649ed486ca06742
Andrey Seleznov <andrewaka39@gmail.com>
2015-02-05 23:37:33 +0200

    список категорий. мелочи на главной странице

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
   def index
     @regions ||= Region.all
     @cities  ||= City.all.to_a
-    @cities  ||= City.joins(:region).where('regions.uk_name = ?', params[:region]).to_a if params[:region].present?
+    @cities  = City.joins(:region).where('regions.uk_name = ?', params[:region]).to_a if params[:region].present?
     @city_rating = CityRating.new @cities
     @cities.sort! {|x,y| @city_rating.position_of(x) <=> @city_rating.position_of(y)}
   end
