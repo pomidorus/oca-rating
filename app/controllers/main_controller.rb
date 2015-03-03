87acc2328556e7006fd0aaf7c7c701bdc83f80c2
Andrey Seleznov <andreii.seleznov@gmail.com>
2015-03-03 15:39:16 +0200

    главная. название области

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,13 +1,15 @@
 class MainController < ApplicationController
   def index
     @regions ||= Region.all
-    @cities  ||= City.all.to_a
 
+    @cities  ||= City.all.to_a
     @cities  = City.joins(:region).where('regions.uk_name = ?', params[:region]).to_a if params[:region].present?
+    @first_city = @cities[0]
+
+    @region_header = params[:region].present? ? "#{params[:region]} область" : 'Все области'
+    if (params[:region] == 'АР Крим') then @region_header = params[:region] end
 
     @city_rating = CityRating.new @cities
     @cities.sort! {|x,y| @city_rating.position_of(x) <=> @city_rating.position_of(y)}
-
-    @first_city = @cities[0]
   end
 end
